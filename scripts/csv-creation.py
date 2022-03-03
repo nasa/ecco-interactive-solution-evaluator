ef512d1cc1163f0cd7e255e44759ad2629d09c3d
Vincent H <hoangvincent69@gmail.com>
2022-03-03 15:26:07 -0800

    Relocated csv-creation py

--- scripts/csv-creation.py
+++ scripts/csv-creation.py
@@ -14,7 +14,7 @@ for i in range(642):
     dictB = {'Y': b}
 
     dfA = pd.DataFrame(dictA)
-    dfA.to_csv(r'./A_' + str(bin) +'.csv', index=False)
+    dfA.to_csv(r'./graph-data/A_' + str(bin) +'.csv', index=False)
 
     dfB = pd.DataFrame(dictB)
-    dfB.to_csv(r'./B_' + str(bin) +'.csv', index=False)
+    dfB.to_csv(r'./graph-data/B_' + str(bin) +'.csv', index=False)
