d5deb22dcf50d1da9d4752634e0d3b8fe537c7da
Vincent H <hoangvincent69@gmail.com>
2022-03-15 11:41:00 -0700

    Style changes and drawer creation

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /* Set the width of the side navigation to 250px */
-function openNav() {
-  document.getElementById("plotNav").style.width = "250px";
+function openNav(id) {
+  document.getElementById(id).style.width = "250px";
 }
 
 /* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("plotNav").style.width = "0";
+function closeNav(id) {
+  document.getElementById(id).style.width = "0";
 }
